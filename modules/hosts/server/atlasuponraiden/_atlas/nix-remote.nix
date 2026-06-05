e14b5f9bd49831f04bc7a664965a07511b311be0
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-06-04 20:09:22 -0600

    Fixing path

--- modules/hosts/server/atlasuponraiden/_atlas/nix-remote.nix
+++ modules/hosts/server/atlasuponraiden/_atlas/nix-remote.nix
@@ -8,7 +8,7 @@
   users.users.nix-remote = lib.mkIf config.my.host.deploy.enableRemoteUser {
     openssh.authorizedKeys.keyFiles = [
       "${inputs.nix-secrets}/ssh-keys/kamino/atlas_nix.pub"
-      "${inputs.nix-secrets}/ssh-keys/zaphod/atlas_nix.pub"
+      "${inputs.nix-secrets}/ssh-keys/zaphodbeeblebrox/atlas_nix.pub"
     ];
   };
 }
