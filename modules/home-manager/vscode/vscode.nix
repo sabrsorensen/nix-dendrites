d8c0e733dc115f97eb61c9584800f662c7cf75d3
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-06-08 00:54:14 -0600

    Added STM32Cube extensions

--- modules/home-manager/vscode/vscode.nix
+++ modules/home-manager/vscode/vscode.nix
@@ -264,9 +264,25 @@
                 extensions = vscodeData.mkExtensions (
                   vscodeData.defaultExts
                   ++ [
+                    "eclipse-cdt.memory-inspector"
+                    "eclipse-cdt.serial-monitor"
+                    "ms-vscode.cmake-tools"
                     "ms-vscode.cpptools"
                     "ms-vscode.cpptools-extension-pack"
                     "platformio.platformio-ide"
+                    "stmicroelectronics.stm32-vscode-extension"
+                    "stmicroelectronics.stm32cube-ide-build-analyzer"
+                    "stmicroelectronics.stm32cube-ide-build-cmake"
+                    "stmicroelectronics.stm32cube-ide-bundles-manager"
+                    "stmicroelectronics.stm32cube-ide-clangd"
+                    "stmicroelectronics.stm32cube-ide-core"
+                    "stmicroelectronics.stm32cube-ide-debug-core"
+                    "stmicroelectronics.stm32cube-ide-debug-generic-gdbserver"
+                    "stmicroelectronics.stm32cube-ide-debug-jlink-gdbserver"
+                    "stmicroelectronics.stm32cube-ide-debug-stlink-gdbserver"
+                    "stmicroelectronics.stm32cube-ide-project-manager"
+                    "stmicroelectronics.stm32cube-ide-registers"
+                    "stmicroelectronics.stm32cube-ide-rtos"
                   ]
                 );
                 keybindings = vscodeData.defaultKeyBindings ++ [ ];
