7bd2b9d9282af03d12b7f248736828f880d6c1da
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-06-11 14:57:25 -0600

    Updated flake.lock and ran nix fmt

--- modules/programs/demlo/_demlo-home.nix
+++ modules/programs/demlo/_demlo-home.nix
@@ -2,21 +2,18 @@
 let
   scriptDir = ./demlo/scripts;
   scriptEntries = builtins.readDir scriptDir;
-  scriptFiles = builtins.attrNames (
-    lib.filterAttrs (_name: type: type == "regular") scriptEntries
-  );
+  scriptFiles = builtins.attrNames (lib.filterAttrs (_name: type: type == "regular") scriptEntries);
 in
 {
   flake.modules.homeManager.demlo = {
-    xdg.configFile =
-      {
-        "demlo/config.lua".source = ./demlo/config.lua;
-      }
-      // builtins.listToAttrs (
-        map (name: {
-          name = "demlo/scripts/${name}";
-          value.source = scriptDir + "/${name}";
-        }) scriptFiles
-      );
+    xdg.configFile = {
+      "demlo/config.lua".source = ./demlo/config.lua;
+    }
+    // builtins.listToAttrs (
+      map (name: {
+        name = "demlo/scripts/${name}";
+        value.source = scriptDir + "/${name}";
+      }) scriptFiles
+    );
   };
 }
