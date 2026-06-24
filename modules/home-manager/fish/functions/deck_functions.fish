1554a2ecfb4be4012b444de248634eac4b9a2502
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-06-24 15:15:23 -0600

    Fixing Deck MOTD cpu temp

--- modules/home-manager/fish/functions/deck_functions.fish
+++ modules/home-manager/fish/functions/deck_functions.fish
@@ -5,10 +5,29 @@ function deckStatus -d "Show Steam Deck system status"
     if test -z "$host_name"
         set host_name (uname -n)
     end
+    set -l cpu_temp (
+        sensors 2>/dev/null | awk '
+            /^acpitz-acpi-0$/ { in_acpitz = 1; next }
+            /^[[:alnum:]_.:-]+$/ && $0 != "acpitz-acpi-0" { in_acpitz = 0 }
+            in_acpitz && /temp1:/ { print $2; exit }
+        '
+    )
+    if test -z "$cpu_temp"
+        set cpu_temp (
+            sensors 2>/dev/null | awk '
+                /^k10temp-pci-/ { in_k10temp = 1; next }
+                /^[[:alnum:]_.:-]+$/ && $1 !~ /^k10temp-pci-/ { in_k10temp = 0 }
+                in_k10temp && /Tctl:/ { print $2; exit }
+            '
+        )
+    end
+    if test -z "$cpu_temp"
+        set cpu_temp "N/A"
+    end
 
     echo "🎮 Steam Deck Status ($host_name):"
     echo "Battery: $(cat /sys/class/power_supply/BAT*/capacity 2>/dev/null || echo "N/A")%"
-    echo "CPU Temp: $(sensors 2>/dev/null | grep 'Tctl:' | awk '{print $2}' || echo "N/A")"
+    echo "CPU Temp: $cpu_temp"
     echo "GPU: $(lspci | grep VGA | cut -d: -f3)"
     echo "Disk: $(df -h /home | tail -1 | awk '{print $3 "/" $2 " (" $5 ")"}')"
 end
