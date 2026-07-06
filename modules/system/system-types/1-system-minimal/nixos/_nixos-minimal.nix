13729ae9b9b83f767f3cc041d778142224be08b7
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-07-06 17:18:07 -0600

    Added Attic pubkey

--- modules/system/system-types/1-system-minimal/nixos/_nixos-minimal.nix
+++ modules/system/system-types/1-system-minimal/nixos/_nixos-minimal.nix
@@ -118,6 +118,7 @@
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM"
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "atlas:YR+4cS0G0pzDX7vHi3Y3/vRBHiftc1WJC9R/g+tYeYA="
           ];
           trusted-users = [ "@wheel" ] ++ lib.optionals enableNixRemote [ "nix-remote" ];
           warn-dirty = false;
