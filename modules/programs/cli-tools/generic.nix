61b1ec27c89163f488387bff418fae876002e20b
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-07-06 13:28:26 -0600

    Adding lease_editor to PATH

--- modules/programs/cli-tools/generic.nix
+++ modules/programs/cli-tools/generic.nix
@@ -10,13 +10,28 @@ let
     let
       system = pkgs.stdenv.hostPlatform.system;
       hasLocalFlake = config.my.host.deploy.localFlakePath != null;
-      localFlakeToolPackages = lib.optionals hasLocalFlake [
-        pkgs.just
-        inputs.self.formatter.${system}
-        inputs.self.packages.${system}.write-flake
-        inputs.self.packages.${system}.write-inputs
-        inputs.self.packages.${system}.write-lock
-      ];
+      hasLocalGuiFlake = hasLocalFlake && config.my.host.features.gui;
+      leasesEditorPython = pkgs.python3.withPackages (
+        pythonPkgs: with pythonPkgs; [
+          pyqt5
+        ]
+      );
+      leasesEditor = pkgs.writeShellApplication {
+        name = "leases_editor.py";
+        runtimeInputs = [ leasesEditorPython ];
+        text = ''
+          exec ${leasesEditorPython}/bin/python ${../../services/dhcp-coredns/leases_editor.py} "$@"
+        '';
+      };
+      localFlakeToolPackages =
+        lib.optionals hasLocalFlake [
+          pkgs.just
+          inputs.self.formatter.${system}
+          inputs.self.packages.${system}.write-flake
+          inputs.self.packages.${system}.write-inputs
+          inputs.self.packages.${system}.write-lock
+        ]
+        ++ lib.optionals hasLocalGuiFlake [ leasesEditor ];
     in
     {
       environment.systemPackages =
