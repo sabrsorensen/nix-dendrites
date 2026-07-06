ee0ecc84d962d9e4f435d053a4310fdd3eb9abae
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-07-06 13:42:57 -0600

    Fixing deps

--- modules/programs/cli-tools/generic.nix
+++ modules/programs/cli-tools/generic.nix
@@ -11,6 +11,14 @@ let
       system = pkgs.stdenv.hostPlatform.system;
       hasLocalFlake = config.my.host.deploy.localFlakePath != null;
       hasLocalGuiFlake = hasLocalFlake && config.my.host.features.gui;
+      qtPluginPath = lib.concatStringsSep ":" [
+        "${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+        "${pkgs.qt5.qtwayland}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+      ];
+      qtQmlPath = lib.concatStringsSep ":" [
+        "${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtQmlPrefix}"
+        "${pkgs.qt5.qtwayland}/${pkgs.qt5.qtbase.qtQmlPrefix}"
+      ];
       leasesEditorPython = pkgs.python3.withPackages (
         pythonPkgs: with pythonPkgs; [
           pyqt5
@@ -18,8 +26,14 @@ let
       );
       leasesEditor = pkgs.writeShellApplication {
         name = "leases_editor.py";
-        runtimeInputs = [ leasesEditorPython ];
+        runtimeInputs = [
+          leasesEditorPython
+          pkgs.qt5.qtbase
+          pkgs.qt5.qtwayland
+        ];
         text = ''
+          export QT_PLUGIN_PATH=${lib.escapeShellArg qtPluginPath}
+          export QML2_IMPORT_PATH=${lib.escapeShellArg qtQmlPath}
           exec ${leasesEditorPython}/bin/python ${../../services/dhcp-coredns/leases_editor.py} "$@"
         '';
       };
