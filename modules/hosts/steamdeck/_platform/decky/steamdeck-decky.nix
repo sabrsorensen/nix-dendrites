96b063325517d8e0bb8b6d1db5009d0338758e99
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-07-13 18:29:04 -0600

    Adding pnpm insecure package for Decky

--- modules/hosts/steamdeck/_platform/decky/steamdeck-decky.nix
+++ modules/hosts/steamdeck/_platform/decky/steamdeck-decky.nix
@@ -230,6 +230,11 @@ in
       extraPythonPackages = deckyLoaderExtraPythonPackages;
     };
 
+    nixpkgs.config.permittedInsecurePackages = [
+      "pnpm-9.15.9"
+    ];
+
+
     # Decky integrates with Steam's embedded browser and expects this toggle to
     # exist before startup on some hosts.
     systemd.services.steam-cef-debug = lib.mkIf config.jovian.decky-loader.enable {
