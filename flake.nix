7ed7346e289274566a7eb5a9d94dbf3a68e04a1d
sabrsorensen <825813+sabrsorensen@users.noreply.github.com>
2026-09-18 18:45:32 -0600

    Updated flake.lock

--- flake.nix
+++ flake.nix
@@ -67,13 +67,6 @@
       url = "github:Jovian-Experiments/Jovian-NixOS";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    lazyvim = {
-      url = "github:pfassina/lazyvim-nix";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
     nix-auto-follow = {
       url = "github:fzakaria/nix-auto-follow";
       inputs.nixpkgs.follows = "nixpkgs";
